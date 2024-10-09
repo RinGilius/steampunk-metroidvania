084c1d1c38a3955fda49533330f40944fa48cd08
RinGilius <zbxfgkiloz123@proton.me>
2024-10-09 13:18:32 +0300

    2д
    топ даун на свалку

--- Rouglike/Scripts/player.gd
+++ Rouglike/Scripts/player.gd
@@ -1,9 +1,19 @@
 extends CharacterBody2D
 
 var speed = 10000
+
+var jump_velocity = -25000
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _ready():
 	position = GameManager.change_position
 func _physics_process(delta):
-	var direction = Input.get_vector("player_left","player_right","player_up","player_down")
-	velocity = direction * speed * delta
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	if Input.is_action_just_pressed("player_jump"):
+		if is_on_floor():
+			velocity.y = jump_velocity * delta
+	var direction = Input.get_axis("player_left","player_right")
+	velocity.x = direction * speed * delta
 	move_and_slide()
