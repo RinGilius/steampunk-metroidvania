bbf5c8a2d5c1832986c960b1aeebfe100e92e6be
RinGilius <zbxfgkiloz123@proton.me>
2024-10-09 15:42:46 +0300

    Анимки

--- Rouglike/Scripts/move_box.gd
+++
@@ -1,6 +0,0 @@
-extends CharacterBody2D
-
-@onready var player = $"../Player"
-func  _process(delta):
-	if Input.is_action_pressed("is_move_box", 1):
-		velocity.x = player.velocity.x
--- Rouglike/Scripts/player.gd
+++ Rouglike/Scripts/player.gd
@@ -2,11 +2,14 @@ extends CharacterBody2D
 
 var speed = 10000
 
+@onready var anim = $AnimatedSprite2D
+
 var jump_velocity = -25000
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _ready():
 	position = GameManager.change_position
+	anim.play("player_idle")
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -16,4 +19,15 @@ func _physics_process(delta):
 			velocity.y = jump_velocity * delta
 	var direction = Input.get_axis("player_left","player_right")
 	velocity.x = direction * speed * delta
+	animate()
 	move_and_slide()
+
+func animate():
+	if velocity.x > 0:
+		anim.flip_h = false
+	elif velocity.x < 0:
+		anim.flip_h = true
+	if velocity.x:
+		anim.play("player_move")
+	else:
+		anim.play("player_idle")
