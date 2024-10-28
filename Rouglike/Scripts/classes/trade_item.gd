d08265969499c548e55ea9adafe2a7d6aa9e9c73
gggdeff <136966959+gggdeff@users.noreply.github.com>
2024-10-28 13:05:35 +0300

    trade base

---
+++ Rouglike/Scripts/classes/trade_item.gd
@@ -0,0 +1,9 @@
+extends Node
+class_name trade_items
+
+
+@export var _name := ""
+
+@export var icon : Texture2D
+
+@export var cost := 1
--- Rouglike/Scripts/global.gd
+++ Rouglike/Scripts/global.gd
@@ -2,3 +2,7 @@ extends Node
 
 
 @export var is_dialog : bool = false
+
+@export var money := 0
+
+@export var items := []
