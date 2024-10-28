fa51c2fa07e8f48ee086bc4b7605c7208dd03342
RinGilius <zbxfgkiloz123@proton.me>
2024-10-28 10:47:31 +0200

    Jump!
    сделал регулируймый прыжок

--- Rouglike/Scripts/player.gd
+++ Rouglike/Scripts/player.gd
@@ -1,76 +1,21 @@
 extends CharacterBody2D
 
-@export var speed = 200
 
-@export var max_speed = 200
+var speed = 7000
 
-@export var acel = 0
-
-@onready var anim = $AnimatedSprite2D
-
-var is_in_des = false
-
-var on_touch = false
-
-@export var jump_velocity = -300
+var jump_velocity = -400
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-func _ready():
-	position = GameManager.change_position
-	anim.play("player_idle")
+
+
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	if Input.is_action_just_pressed("enteract") and g.is_dialog == false:#'''an#'''and g.is_dialog == false and is_death == false and g.is_cutscene == false and velocity == Vector2.ZERO'''d g.is_dialog == false and is_death == false and g.is_cutscene == false and velocity == Vector2.ZERO'''
-			var ent = %enteract.get_overlapping_areas()
-			if ent.size() > 0:
-				ent[0].action()
-				return
-	if g.is_dialog == false:
-		if Input.is_action_just_pressed("player_jump") && is_on_floor():
-			velocity.y = jump_velocity
-			
-		var direction = Input.get_axis("player_left","player_right")
-		if direction:
-			speed += 1.5
-			velocity.x = direction * speed
-		else:
-			velocity.x = move_toward(velocity.x,0,speed)
-			speed = 125
-			
-		if speed >= max_speed:
-			speed = max_speed
-		move_and_slide()
-				
-	if Input.is_action_just_pressed("player_jump"):
-		if is_on_floor():
-			velocity.y = jump_velocity * delta
-	var direction = Input.get_axis("player_left","player_right")
+	if Input.is_action_just_released("player_jump") and velocity.y < 0:
+		velocity.y = jump_velocity / 4
+	if Input.is_action_just_pressed("player_jump") and is_on_floor():
+		velocity.y = jump_velocity
+	var direction = Input.get_axis("move_left", "move_right")
 	velocity.x = direction * speed * delta
-	destructive()
-	animate()
-	
 
-func animate():
-	if velocity.x > 0:
-		anim.flip_h = false
-	elif velocity.x < 0:
-		anim.flip_h = true
-	if velocity.x:
-		anim.play("player_move")
-	else:
-		anim.play("player_idle")
-
-
-func _on_area_enemys_area_entered(area):
-	if area.name == "Area_Destructive":
-		is_in_des = true
-	
-
-func _on_area_enemys_area_exited(area):
-	if area.name == "Area_Destructive":
-		is_in_des = false
-
-func destructive():
-	if is_in_des == true and Input.is_action_just_pressed("is_move_box"):
-		$"../DestructiveObject".queue_free()
+	move_and_slide()
