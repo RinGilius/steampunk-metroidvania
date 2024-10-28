d4165ead70bd08ee753d359044b70879c707faa5
gggdeff <136966959+gggdeff@users.noreply.github.com>
2024-10-28 12:44:52 +0300

    player is class now

--- Rouglike/Scripts/Perehod.gd
+++ Rouglike/Scripts/Perehod.gd
@@ -4,9 +4,10 @@ extends Area2D
 
 @export var change_position = Vector2()
 
-@onready var player = $Player
+#@onready var player = $Player
 
 func _on_body_entered(body):
-	get_tree().change_scene_to_file(change_scene)
-	GameManager.change_position = change_position
+	if body is player:
+		get_tree().change_scene_to_file(change_scene)
+		GameManager.change_position = change_position
 	
